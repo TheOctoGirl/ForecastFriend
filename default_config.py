d34669c41addbc723c32a7f4091c4dc2b820baea
TheOctoGirl <119755793+TheOctoGirl@users.noreply.github.com>
2023-05-30 16:53:16 -0700

    Add default config

---
+++ default_config.py
@@ -0,0 +1,6 @@
+discord_api_key = "DISCORD_API_KEY_HERE"
+
+
+# This option will only be used if you want a fallback if open street map geocoding fails
+use_openweathermap_geocoding = False
+open_weathermap_api_key = "OPEN_WEATHERMAP_API_KEY_HERE"
